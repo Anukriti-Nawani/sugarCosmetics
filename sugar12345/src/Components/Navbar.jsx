5d3508dfb59e36b10a9ed71643ebc50c6d0f7e4b
Anukriti-Nawani <anunwn@gmail.com>
2022-08-28 17:11:38 +0530

    project file

---
+++ sugar12345/src/Components/Navbar.jsx
@@ -0,0 +1,95 @@
+import { Link } from "react-router-dom";
+import { FaUser, FaHeart, FaShoppingBag } from "react-icons/fa";
+import { MdLocalOffer } from "react-icons/md";
+import SubNavbar from "./SubNavbar";
+function Navbar() {
+  return (
+    <div>
+      <div
+        style={{
+          display: "flex",
+          width: "100%",
+          height: "80px",
+          backgroundColor: "#efefef"
+        }}
+      >
+        <img
+          style={{ height: "60px", paddingLeft: "60px", paddingTop: "15px" }}
+          src="https://media.sugarcosmetics.com/upload/SUGARLogo1.png"
+          alt=""
+        />
+        <input
+          style={{
+            height: "40px",
+            width: "550px",
+            marginLeft: "80px",
+            marginTop: "20px",
+            border: "none",
+            borderTopLeftRadius: "30px",
+            borderBottomLeftRadius: "30px",
+            paddingLeft: "20px"
+          }}
+          type="text"
+          placeholder='Try "Liquid Lipstick"'
+        />
+        <button
+          style={{
+            border: "none",
+            backgroundColor: "black",
+            height: "43px",
+            width: "120px",
+            marginTop: "20px",
+            color: "white",
+            borderTopRightRadius: "30px",
+            borderBottomRightRadius: "30px",
+            fontSize: "17px"
+          }}
+        >
+          Search
+        </button>
+        <FaUser
+          style={{ marginTop: "30px", marginLeft: "100px", fontSize: "20px" }}
+        />
+        <Link to="/signin">
+          <p
+            style={{
+              textDecoration: "underline",
+              marginLeft: "5px",
+              marginTop: "30px",
+              color: "black"
+            }}
+          >
+            Login/Register
+          </p>
+        </Link>
+        <FaHeart
+          style={{
+            marginTop: "30px",
+            marginLeft: "100px",
+            color: "black",
+            fontSize: "25px"
+          }}
+        />
+        <FaShoppingBag
+          style={{
+            marginTop: "30px",
+            marginLeft: "30px",
+            marginRight: "5px",
+            color: "black",
+            fontSize: "24px"
+          }}
+        />
+        <MdLocalOffer
+          style={{
+            marginTop: "27px",
+            marginLeft: "20px",
+            marginRight: "50px",
+            fontSize: "30px"
+          }}
+        />
+      </div>
+      <SubNavbar />
+    </div>
+  );
+}
+export default Navbar;
